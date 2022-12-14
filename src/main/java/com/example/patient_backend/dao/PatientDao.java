95d87cbfa6e01bb863d1eaec96d3ad529b0b7b04
sherin200 <sherinsebastian21>
2022-12-14 18:51:50 +0530

    dao

--- src/main/java/com/example/patient_backend/dao/PatientDao.java
+++ src/main/java/com/example/patient_backend/dao/PatientDao.java
@@ -1,4 +1,20 @@
 package com.example.patient_backend.dao;
 
-public interface PatientDao {
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface PatientDao extends CrudRepository <Patients,Integer>
+{
+    @Query(value = "SELECT `id`, `address`, `contact`, `dname`, `doa`, `image`, `name`, `pid` FROM `patient` WHERE `pid`=:pid",nativeQuery = true)
+    List<Patients> SearchPatient(@Param("pid") Integer pid);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `patient` WHERE `id`=:id",nativeQuery = true)
+    void DeletePatient(@Param("id") Integer id);
 }
